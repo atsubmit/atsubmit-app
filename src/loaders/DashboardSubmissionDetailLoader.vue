516557a791ccf9edfb373d16ef41093040b2e47f
nguyenhy <nguyenhy311@gmail.com>
2026-03-20 15:05:49 +0700

    fix: correct submission detail props

--- src/loaders/DashboardSubmissionDetailLoader.vue
+++ src/loaders/DashboardSubmissionDetailLoader.vue
@@ -4,7 +4,10 @@ import type { Submission } from "@/pages/DashboardSubmissionDetailPage.types";
 import DashboardSubmissionDetailPage from "@/pages/DashboardSubmissionDetailPage.vue";
 
 const props = defineProps<{
-    submission: Submission;
+    submission: Submission & {
+        form_slug: string;
+        form_name: string;
+    };
 }>();
 </script>
 
